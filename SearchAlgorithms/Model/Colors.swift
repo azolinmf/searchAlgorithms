494669251978ee40b32b17144c92a33222b0c5fa
Maria Fernanda Azolin <azolin.mf@gmail.com>
2020-05-31 16:50:58 -0300

    adds colors model

---
+++ SearchAlgorithms/Model/Colors.swift
@@ -0,0 +1,21 @@
+//
+//  Colors.swift
+//  SearchAlgorithms
+//
+//  Created by Maria Fernanda Azolin on 31/05/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Colors {
+    
+    static let instance = Colors()
+    
+    var orange = UIColor(red: 255/255.0, green: 149/255.0, blue: 0/255.0, alpha: 1.0)
+    var yellow = UIColor(red: 238/255, green: 220/255, blue: 19/255, alpha: 1)
+    var transparentOrange = UIColor(red: 255/255, green: 149/255, blue: 0/255, alpha: 0.3)
+    var purple = UIColor(red: 44/255, green: 40/255, blue: 57/255, alpha: 1)
+    var gray = UIColor(displayP3Red: 95/255, green: 95/255, blue: 95/255, alpha: 1.0)
+}
